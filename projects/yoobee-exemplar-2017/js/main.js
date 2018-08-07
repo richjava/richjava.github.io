af4de8a115aa994126f9012c98018be9a6178e71
Richard Lovell <090392@myacg.org>
2018-08-07 14:58:54 +1200

    Added Yoobee exemplar work 2017 project

---
+++ projects/yoobee-exemplar-2017/js/main.js
@@ -0,0 +1,44 @@
+$(function() {
+    $('.column').hover(function(){
+        $(this).find('h3').show();
+    },function(){
+        $(this).find('h3').hide();
+    });
+    
+    //flipping heading from http://codepen.io/mohaiman/pen/MpZgpJ 
+    //TODO needs to be refactored
+    var word = ['Yoobee Web & UX Design', 'Graduating classes of 2017', 'Student Portfolios'];
+      var element = document.getElementsByTagName('h1')[0];
+      var wordIndex = 1;
+      var timeout = 20000;//20 seconds
+      var resetTimeout = 1000;
+
+      var resetAnimation = function() {
+        element.classList.remove('flip');
+      }
+
+      setInterval(function(){
+          switch (wordIndex) {
+            case 0:
+              element.classList.add('flip');
+              element.textContent = word[wordIndex];
+              wordIndex = 1;
+              setTimeout(resetAnimation, resetTimeout);
+            break;
+
+            case 1:
+              element.classList.add('flip');
+              element.textContent = word[wordIndex];
+              wordIndex = 2;
+              setTimeout(resetAnimation, resetTimeout);
+            break;
+
+            case 2:
+              element.classList.add('flip');
+              element.textContent = word[wordIndex];
+              wordIndex = 0;
+              setTimeout(resetAnimation, resetTimeout);
+            break;
+          }
+      },timeout)
+});
