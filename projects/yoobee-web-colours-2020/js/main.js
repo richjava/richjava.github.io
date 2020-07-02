98239acd896db0f73cf9e121a4bef09c916463ab
Richard Lovell <richard.lovell@yoobeecolleges.com>
2020-07-02 15:44:39 +1200

    Added new class

---
+++ projects/yoobee-web-colours-2020/js/main.js
@@ -0,0 +1,43 @@
+$(function() {
+    $('.column').hover(function(){
+        $(this).find('h3').show();
+    },function(){
+        $(this).find('h3').hide();
+    });
+    
+    //TODO needs to get from config
+    var word = ['19CDWUX07', 'Graduating class of 2020', 'Yoobee Christchurch Web'];
+      var element = document.getElementsByTagName('h1')[0];
+      var wordIndex = 1;
+      var timeout = 20000;//20 seconds
+      var resetTimeout = 1000;
+
+      var resetAnimation = function() {
+        element.classList.remove('flip');
+      }
+
+      setInterval(function(){
+          switch (wordIndex) {
+            case 0:
+              element.classList.add('flip');
+              element.textContent = word[wordIndex];
+              wordIndex = 1;
+              setTimeout(resetAnimation, resetTimeout);
+            break;
+
+            case 1:
+              element.classList.add('flip');
+              element.textContent = word[wordIndex];
+              wordIndex = 2;
+              setTimeout(resetAnimation, resetTimeout);
+            break;
+
+            case 2:
+              element.classList.add('flip');
+              element.textContent = word[wordIndex];
+              wordIndex = 0;
+              setTimeout(resetAnimation, resetTimeout);
+            break;
+          }
+      },timeout)
+});
